19049b71bb618b632534820cdaaa814a14679a78
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-10-08 14:30:15 +0200

    Création des routes users et pour ls recettes

---
+++ api/routes/recipeRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+const {createRecipe,
+    getAllRecipes,
+    getRecipeById,
+    updateRecipe,
+    deleteRecipe,} = require('../controllers/recipeController')
+const authMiddleware = require('../middleware/authMiddleware')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png|gif/;
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedTypes.test(file.mimetype);
+    if (extname && mimetype) {
+        return cb(null, true);
+    }
+    cb(new Error('Seules les images sont autorisées (jpeg, jpg, png, gif).'));
+};
+
+const upload = multer({ storage, fileFilter });
+
+router.post('/', authMiddleware, upload.single('image'), createRecipe)
+router.get('/', getAllRecipes)
+router.get('/:id', getRecipeById)
+router.put('/:id', authMiddleware, upload.single('image'), updateRecipe)
+router.delete('/:id', authMiddleware, deleteRecipe)
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -3,12 +3,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes')
+const recipeRoutes = require('./routes/recipeRoutes')
 
 dotenv.config()
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/recipes', recipeRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
