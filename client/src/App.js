a824b477af4ce8e53b568271d56231fada7ce465
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-10-16 14:11:25 +0200

    Création du header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login';
+import RecipeList from './pages/RecipeList/RecipeList';
+import RecipeDetail from './pages/RecipeDetail/RecipeDetail';
+import CreateRecipe from './pages/CreateRecipe/CreateRecipe';
+import EditRecipe from './pages/EditRecipe/EditRecipe';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <Header/>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/recipes' element={<RecipeList/>} />
+          <Route path='/recipes/:id' element={<RecipeDetail/>} />
+          <Route path='/create-recipe' element={<CreateRecipe/>} />
+          <Route path='/edit-recipe' element={<EditRecipe/>}/>
+        </Routes>
+      </main>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+// src/components/Header/Header.js
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Header.css';
 
-const Header = () => {
+function Header() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
+
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Carnet de Recettes</Link>
+      </div>
+      <nav>
+        <ul>
+          <li><Link to="/recipes">Recettes</Link></li>
+          {token ? (
+            <>
+              <li><Link to="/create-recipe">Ajouter Recette</Link></li>
+              <li><button onClick={handleLogout} className="logout-button">Déconnexion</button></li>
+            </>
+          ) : (
+            <>
+              <li><Link to="/register">Inscription</Link></li>
+              <li><Link to="/login">Connexion</Link></li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
