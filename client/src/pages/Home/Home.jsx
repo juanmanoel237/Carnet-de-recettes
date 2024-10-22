cd47deb96018d5ab7e64db3d95075f525779b74c
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-10-22 12:41:22 +0200

    Fin du composant home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import homeImage from '../../assets/images/i103208-photo-de-pot-au-feu.webp'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="home-content">
+        <h1>Bienvenue dans votre carnet de recettes</h1>
+        <p>Créez, partagez et explorez des recettes facilement !</p>
+        <Link to='/recipes' className='btn'>Voir les recettes</Link>
+      </div>
+      <div className="home-image">
+        <img src={homeImage} alt="Recettes" />
+      </div>
+    </div>
   )
 }
 
