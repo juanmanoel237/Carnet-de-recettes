d7d2b98842ecb26464681a8f233e6292ce267f9b
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-10-22 13:16:05 +0200

    Fin du formulaire de connexion

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    email:'',
+    password:''
+  })
+
+  const [error, setError] = useState('');
+  const {email, password} = formData
+
+  const handleChange = (e)=>{
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    setError('')
+    try{
+    const res = await axios.post('http://localhost:5000/api/auth/login', formData)
+    localStorage.setItem('token', res.data.token)
+    navigate('/recipes');
+    }
+    catch(err){
+      setError(err.response?.data?.message,'Erreur lors de l\'inscription.')
+    }
+  }
+
   return (
-    <div>Login</div>
+    <div className='register-container'>
+      <form className='register-form' onSubmit={handleSubmit}>
+        <h2>Connexion</h2>
+        {error && <div className='error'>{error}</div>}
+        <div className='form-group'>
+          <label htmlFor="email">Adresse Email</label>
+          <input 
+          type="text"
+          id='email'
+          name='email'
+          value={email}
+          onChange={handleChange}
+          required
+          placeholder='Entrez votre adresse mail'
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Mot de Passe</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            required
+            placeholder="Entrez votre mot de passe"
+          />
+        </div>
+        <button type='submit' className='btn'>Se Connecter</button>
+      </form>
+    </div>
   )
 }
 
